44f6069177344bee5d2302a412dc65d6f9d7177c
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2024-11-01 20:00:26 +0100

    first commit https://www.youtube.com/watch\?v\=VljhZ0e9zGE\&t\=1235s untill minute 14

---
+++ lua/keymaps.lua
@@ -0,0 +1,7 @@
+-- space bar leader key
+vim.g.mapleader = " "
+
+-- buffers
+vim.keymap.set("n", "<leader>n", ":bn<cr>")
+vim.keymap.set("n", "<leader>p", ":bp<cr>")
+vim.keymap.set("n", "<leader>x", ":bd<cr>")
